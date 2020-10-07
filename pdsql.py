d8b2e379cf84b896effff7934aa696fd058efa67
rakjido <ipos222@naver.com>
2020-10-07 11:02:25 +0900

    2020.10.07 where

--- pdsql.py
+++ pdsql.py
@@ -20,28 +20,38 @@ def pdsql(SELECT, FROM, WHERE=None, ORDER_BY=None) -> pd.DataFrame:
         print("'FROM' type should be Pandas Dataframe")
         return None
     
+    FROM.columns = map(str.lower, FROM.columns)
+
     if(WHERE != None):
         if not isinstance(WHERE, list):
             print("'WHERE' type should be List")
             return None
         else:
+            _condition = []
 
-            # 순서가 중요. >=,<=가 나머지 보다 앞에 있어야 한다.
-            _keyword =['>=', '<=', '>', '<', '=']
+            for i, _str in enumerate(WHERE):
+            #    print("{} : {}".format(i,_str))
 
-            for key in _keyword:
-            #    print(WHERE[0].find(key))
-                if (WHERE[0].find(key)>0):
-                    cond = None
-                    cond=WHERE[0].split(key)
-                    if (key =='='):
-                        key='=='
-                    cond.insert(1, key)
-                    print("FROM['" + cond[0].strip() + "']" + cond[1] + cond[2])
-                    filter1 = eval("FROM['" + cond[0].strip() + "']" + cond[1] + cond[2])
-                    
-                    FROM = FROM.loc[filter1,:]
-                    break
+                # 순서가 중요. >=,<=가 나머지 보다 앞에 있어야 한다.
+                _keyword =['or','and','>=', '<=', '>', '<', '=']
+                _pair = {'or': '|', 'and': '&'}
+
+                for key in _keyword:
+                    if (_str.lower().find(key)>=0):
+                        cond = None
+                        if(key != 'or' and key != 'and'):
+                            cond = _str.lower().split(key)
+                            if (key =='='):
+                                key='=='
+                            cond.insert(1, key)
+                            _condition.append("( FROM['" + cond[0].strip() + "']" + cond[1] + cond[2] + ") ") 
+                        elif(key == 'or' or key == 'and') :
+                            _condition.append(_pair[key.lower()] + " ")        
+
+                        break
+
+            print("".join(_condition))
+            FROM = FROM.loc[eval("".join(_condition)),:]
 
     if not isinstance(SELECT, list):
         print("'SELECT' type should be List")
@@ -54,3 +64,4 @@ def pdsql(SELECT, FROM, WHERE=None, ORDER_BY=None) -> pd.DataFrame:
 
             
     return FROM
+
